47b45c54fb3399cc1b489a8e611e6c6532c74e7c
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2022-05-23 23:24:15 +0200

    Add dynamic grid

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,21 @@
 
-let gridSize = 256;
-for (let i = 0; i < gridSize; i++) {
-    let tag = document.createElement("div");
-    tag.id = i;
-    tag.className = "gridbox";
-    grid.append(tag);
+let horizontalCells = 50;
+let verticalCells = 50;
 
+for (let i = 0; i < horizontalCells; i++) {
+    let horizontalCell = document.createElement("div");
+    horizontalCell.id = i;
+    container.append(horizontalCell);
+
+    for (let j = 0; j < verticalCells; j++) {
+        let verticalCell = document.createElement("div");
+        verticalCell.id = `h${i}w${j}`;
+        verticalCell.className = "cell";
+        horizontalCell.append(verticalCell);
+    }
 }
 
-document.querySelectorAll('.gridbox').forEach(item => {
+document.querySelectorAll('.cell').forEach(item => {
     item.addEventListener('mouseenter', event => {
         item.classList.add("hover");
     })

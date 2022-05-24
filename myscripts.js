6951c96ae9248d15c8ffd1396deef70b616ba105
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2022-05-24 21:19:53 +0200

    Complete exercise
    Add New grid-button and prompts to ask for the grid size. Add function
    to change the color of the cell on mouseover.

--- myscripts.js
+++ myscripts.js
@@ -1,22 +1,41 @@
+document.getElementById("btn").addEventListener("click", newGrid); // create a new grid when pressing the button
 
-let horizontalCells = 50;
-let verticalCells = 50;
-
-for (let i = 0; i < horizontalCells; i++) {
-    let horizontalCell = document.createElement("div");
-    horizontalCell.id = i;
-    container.append(horizontalCell);
-
-    for (let j = 0; j < verticalCells; j++) {
-        let verticalCell = document.createElement("div");
-        verticalCell.id = `h${i}w${j}`;
-        verticalCell.className = "cell";
-        horizontalCell.append(verticalCell);
+function newGrid(){ // create a new grid
+    let horizontalCells = prompt("How many horizontal cells?"); // create horizontal cells
+    while (horizontalCells > 100) {  
+        horizontalCells = prompt("Please enter a number between 1 and 100!"); 
+    } 
+    
+    let verticalCells = prompt("How many vertical cells?"); // create vertical cells
+    while (verticalCells > 100) {  
+        verticalCells = prompt("Please enter a number between 1 and 100!");  
+    } 
+    
+    let container = document.getElementById("container"); // remove the cells from the container
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
+    
+    for (let i = 0; i < horizontalCells; i++) { // create horizontal cells
+        let horizontalCell = document.createElement("div");
+        horizontalCell.className = "horizontal-cell";
+        // horizontalCell.id = i;
+        container.append(horizontalCell);
+    
+        for (let j = 0; j < verticalCells; j++) { // create vertical cells
+            let verticalCell = document.createElement("div");
+            // verticalCell.id = `h${i}w${j}`;
+            verticalCell.className = "vertical-cell";
+            horizontalCell.append(verticalCell);
+        }
+    }
+    
+    document.querySelectorAll('.vertical-cell').forEach(item => { // change color of the vertical cell when hovering with mouse
+        item.addEventListener('mouseenter', event => {
+            item.classList.add("hover"); // adds color by adding .hover-class
+            // item.style.backgroundColor = "#" + ((1<<24)*Math.random() | 0).toString(16); // adds random color
+        })
+      })
 }
 
-document.querySelectorAll('.cell').forEach(item => {
-    item.addEventListener('mouseenter', event => {
-        item.classList.add("hover");
-    })
-  })
+
